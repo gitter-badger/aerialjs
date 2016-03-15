412eee5b723f819b2645be94739eb6f02a3d4f4e
javiergarciagonzalez <javier.garciagonzalez@bq.com>
2016-03-15 10:59:24 +0100

    Created package.js with package description

---
+++ package.js
@@ -0,0 +1,24 @@
+Package.describe({
+    name: 'bquarks:aerialjs',
+    version: '0.0.1',
+    // Brief, one-line summary of the package.
+    summary: 'Meteor package to connect web apps with corbel platform',
+    // URL to the Git repository containing the source code for this package.
+    git: 'git@github.com:bquarks/aerialjs.git',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+    api.versionsFrom('1.2.1');
+    api.use('ecmascript');
+    api.addFiles('aerialjs.js');
+});
+
+Package.onTest(function(api) {
+    api.use('ecmascript');
+    api.use('tinytest');
+    api.use('bquarks:aerialjs');
+    api.addFiles('aerialjs-tests.js');
+});

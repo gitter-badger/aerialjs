7213b16d83ef7082b1e14a1ff3b84dcd95afd7a2
Ivan Rodriguez <ivan.rodriguez@bq.com>
2016-03-22 16:43:43 +0100

    Add es6 classes & jscsrc jshintrc
    Change-Id: I5f0b2a46f4eac2dcf6434406010c08570196e586

--- package.js
+++ package.js
@@ -1,35 +1,37 @@
 Package.describe({
-    name: 'bquarks:aerialjs',
-    version: '0.0.1',
-    // Brief, one-line summary of the package.
-    summary: 'Meteor package to connect web apps with corbel platform',
-    // URL to the Git repository containing the source code for this package.
-    git: 'https://github.com/bquarks/aerialjs.git',
-    // By default, Meteor will default to using README.md for documentation.
-    // To avoid submitting documentation, set this field to null.
-    documentation: 'README.md'
+  name: 'bquarks:aerialjs',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'Meteor package to connect web apps with corbel platform',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/bquarks/aerialjs.git',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
 });
 
 Npm.depends({
-    'corbel-js': '0.4.0'
+  'corbel-js': '0.4.0'
 });
 
 Package.onUse(function(api) {
-    api.versionsFrom('1.2.1');
-    api.use('ecmascript');
+  api.versionsFrom('1.2.1');
+  api.use([
+    'ecmascript',
+  ]);
 
-    api.addFiles('lib/aerialjs.js', ['client','server']);
-    api.addFiles('lib/collection.js', ['client','server']);
-    api.addFiles('lib/common.js');
+  api.addFiles('lib/aerialjs.js', ['client', 'server']);
+  api.addFiles('lib/collection.js', ['client', 'server']);
+  api.addFiles('lib/common.js');
 
-    api.export('Aerial', ['client','server']);
+  api.export('Aerial', ['client', 'server']);
 
 });
 
 Package.onTest(function(api) {
-    api.use(["mike:mocha-package", "practicalmeteor:chai", "practicalmeteor:sinon"]);
+  api.use(["mike:mocha-package", "practicalmeteor:chai", "practicalmeteor:sinon"]);
 
-    api.use('bquarks:aerialjs');
-    // Add test files, eg:
-    api.addFiles('tests/aerial-test.js');
+  api.use('bquarks:aerialjs');
+  // Add test files, eg:
+  api.addFiles('tests/aerial-test.js');
 });
